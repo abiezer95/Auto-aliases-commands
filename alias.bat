a553e7e1dbc4c1cc61ccfef4ebdb64a9c21695b9
abiezer <areyes@farmahorro.com.do>
2019-02-08 09:47:58 -0400

    First Doskey commands

---
+++ alias.bat
@@ -0,0 +1,18 @@
+doskey .=cd
+doskey l=dir
+doskey art-start=php artisan serve
+doskey art:seed=php artisan db:seed
+doskey artm:mseed=php artisan make:seeder
+doskey artm:mt=php artisan make:test
+doskey art:mi=php artisan migrate
+doskey art:fr=php artisan migrate:fresh
+doskey art:ref=php artisan migrate:refresh
+doskey art:re=php artisan migrate:reset
+doskey cm=commit -m
+doskey st=git status
+doskey push=git push origin
+doskey push=git push origin
+doskey pull=git pull
+doskey aa=git add -A
+doskey mg=git merge
+echo listo
